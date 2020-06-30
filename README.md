112e7f2d40c006ab23ec54477f79f40803c8697b
Lycurgus1 <max.palmer.official@outlook.com>
2020-06-30 17:22:20 +0100

    Python variables

---
+++ README.md
@@ -0,0 +1,9 @@
+#Documentation for Python Variables
+
+***What they are***
+- Empty box used to store data
+- 
+
+***Types***
+- test
+- Cannot add integer/float to string
---
+++ variables.py
@@ -0,0 +1,48 @@
+# print("Hello, World")
+
+# Create variable
+
+# x = 10  # Integer
+# y = 3.6  # Float
+# z = "Test"  # String
+#
+# result = x + y
+
+# print(result)
+
+# Built in method called type
+
+# print(type(x))
+
+# Cannot combine string and float. So cast int into string first
+
+# Will not work
+# print(y + z)
+
+# print(str(y) + z)
+
+# Getting input
+# x = input("Enter name")
+# print( x )
+
+# Overriding value
+
+# z = "New word"
+# print(z)
+# z = "New new"
+# print(z)
+
+# Exercise User Details
+
+# Create variable called first_name and last_name
+first_name = input("Enter first name here")
+last_name = input("Enter last name here ")
+
+# Create variable called age
+
+
+# Create variable named address
+
+# Prompt user for information
+
+# Create a variable named full_name and display
