852d1e24252682751269c2d7f2dd296e88a14525
Lycurgus1 <max.palmer.official@outlook.com>
2020-07-02 16:35:49 +0100

    02/07/2020 code

--- 02.07.2020 code.py
+++ 02.07.2020 code.py
@@ -126,49 +126,49 @@ import math
 
 ## Inheritance, polymorhpism, encapsulation, abstraction
 
-class Dog:
-    amimal_kind = "Canine" # Class variable
+# class Dog:
+#     amimal_kind = "Canine" # Class variable
+#
+#     # Init intalizes class
+#     # Give/find name and color for dog
+#     def __init__(self, name, color):
+#         self.name = name
+#         self.color = color
+#
+#     def bark(self):
+#         return "woof, woof"
+#
+# # create a methods inside for sleep, breath, run, eat
+#
+#     # Encapsulation. This creates a private method. Abstraction is a expansion on this
+#     def __sleep(self):
+#         return "zzzzz"
+#
+#     # Pas enables this function without error
+#     def breath(self):
+#         pass
+#
+#     # Inheritance. This refers to creating sub classes, but run is using is a
+#     # previously define attribute
+#     def run(self):
+#         return "Come back, {}".format(self.name)
+#
+#     # Polymorphism. Here the sub class method overrides the methods in the
+#     # parent class when appropriate. It first searches it its own class, then
+#     # in its inherited class
+#     def eat(self):
+#         print("{} is eating fast".format(self.__class__.__name__))
+#
+# class Retriever(Dog):
+#     def eat(self):
+#         print("Retriever is eating slowly")
+#
+# class Labrador(Dog):
+#     pass
 
-    # Init intalizes class
-    # Give/find name and color for dog
-    def __init__(self, name, color):
-        self.name = name
-        self.color = color
-
-    def bark(self):
-        return "woof, woof"
-
-# create a methods inside for sleep, breath, run, eat
-
-    # Encapsulation. This creates a private method. Abstraction is a expansion on this
-    def __sleep(self):
-        return "zzzzz"
-
-    # Pas enables this function without error
-    def breath(self):
-        pass
-
-    # Inheritance. This refers to creating sub classes, but run is using is a
-    # previously define attribute
-    def run(self):
-        return "Come back, {}".format(self.name)
-
-    # Polymorphism. Here the sub class method overrides the methods in the
-    # parent class when appropriate. It first searches it its own class, then
-    # in its inherited class
-    def eat(self):
-        print("{} is eating fast".format(self.__class__.__name__))
-
-class Retriever(Dog):
-    def eat(self):
-        print("Retriever is eating slowly")
-
-class Labrador(Dog):
-    pass
-
-jim = Dog("Canine", "White") # Creating object of our class
-peter = Dog("peter", "Brown")
-
+# # Creating object of our class
+# jim = Dog("Canine", "White")
+# peter = Dog("peter", "Brown")
 # print(jim.name)
 
 ## Testing encapsulation
@@ -178,4 +178,7 @@ peter = Dog("peter", "Brown")
 # print(peter.run())
 
 ## Testing polymorphism
-print()
+# jack = Labrador("Bill", "Black")
+# jill = Retriever("Ben", "Brown")
+# jack.eat()
+# jill.eat()
---
+++ Abstraction_Theory.md
@@ -0,0 +1,37 @@
+### Abstraction
+```python
+def bark(self)
+    return "woof"
+
+print(fido.bark())
+outcome = woof
+```
+
+### Inheritance
+- Animal - from animal class inherited:
+    - Attributes
+    - Functionalities
+
+- Snake inherits everything from animals class
+```python
+from animal import Animal
+```
+- animal is file, Animal is class
+- Snake inherits everything from animal, animal.sleep etc
+
+```python
+def __init__(self)
+    super.init()
+```
+- Super init refers to parent/base class
+
+###Polymorphism
+- We can inherit as well as overide
+
+### Encapsulation
+- Making variables or even methods private
+- Hides apect of class from user
+```python
+def __sleep()
+```
+
